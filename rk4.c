8088bb9aa58223ef72cc40f75fcbf7045c2695c7
tisashrestha <141940566+tisashrestha@users.noreply.github.com>
2024-04-14 13:24:47 +0545

    Update rk4.c

--- rk4.c
+++ rk4.c
@@ -1,1 +1,29 @@
+#include<stdio.h>
+#include<math.h>
 
+float func(float x, float y){
+	return 3*pow(x,2)+1;
+}
+
+int main(){
+	float x0, y0, h, xn, y1, k1, k2, k3, k4, k;
+	int i, n;
+	printf("Enter the values of x and y and stepsize h: ");
+	scanf("%f%f%f",&x0,&y0,&h);
+	printf("Enter the value of x for which y is required: ");
+	scanf("%f",&xn);
+	n = (xn-x0)/h;
+	
+	for(i=0;i<n;i++){
+		k1 = h*func(x0, y0);
+		k2 = h*func(x0+h/2, y0+k1/2);
+		k3 = h*func(x0+h/2, y0+k2/2);
+		k4 = h*func(x0+h, y0+k3);
+		k = (k1+2*k2+2*k3+k4)/6;
+		y1 = y0 + k;
+		y0 = y1;
+		x0 = x0 + h;
+	}
+			printf("The value of y is %f", y1);
+		return 0;
+}
